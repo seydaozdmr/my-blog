dedc618d0faae400e50b6fc39fd93f5b6706658f
ogrseydaozdemir <148540592+ogrseydaozdemir@users.noreply.github.com>
2025-03-25 14:44:51 +0300

    add metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,32 @@ import { Footer } from '@/components/layout/Footer'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'CrownPro',
-  description: 'Yüksek Tırmanış Hizmetleri',
+  title: {
+    template: '%s | CrownPro',
+    default: 'CrownPro - Ana Sayfa',
+  },
+  description: 'Antalya Ağaç Budama Hizmetleri',
+  keywords: ['agac', 'ağaç budama', 'ağaç kesme', 'ağaç bakımı', 'ağaç kesme hizmeti', 'ağaç budama hizmeti', 'ağaç kesme fiyatları', 'ağaç budama fiyatları'],
+  robots: {
+    index: true,
+    follow: true,
+  },
+  openGraph: {
+    title: 'CrownPro',
+    description: 'Agac Budama Hizmetleri',
+    url: 'https://antalyaagacbudama.com',
+    siteName: 'Antalya Ağaç Budama',
+    images: [
+      {
+        url: 'https://antalyaagacbudama.com/_next/image?url=%2Fimages%2Flogo3.png&w=256&q=75',
+        width: 1200,
+        height: 630,
+        alt: 'CrownPro Logo',
+      },
+    ],
+    locale: 'tr_TR',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
