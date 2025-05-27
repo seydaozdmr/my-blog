1df00d87952d7b07a869a4183b50c70a0f44653e
ogrseydaozdemir <148540592+ogrseydaozdemir@users.noreply.github.com>
2025-05-27 13:20:19 +0300

    page seo

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,6 +2,7 @@ import { Metadata } from 'next'
 
 import { ProductList } from './ProductList'
 import { getAllServices } from '@/lib/mdUtils'
+import { PageSEO } from '@/components/SEO/PageSEO'
 
 
 export const metadata: Metadata = {
@@ -13,11 +14,21 @@ export default async function ProductsPage() {
   const services = await getAllServices()
   
   return (
-    <section className="py-12">
-      <div className="container mx-auto px-4">
-        <h1 className="text-4xl font-bold mb-12 text-center">Hizmetlerimiz</h1>
-        <ProductList services={services} />
-      </div>
-    </section>
+    <>
+      <PageSEO
+        title="Ağaç Budama Hizmetleri"
+        description="Antalya'da profesyonel ağaç budama ve kesme hizmetleri. Uzman ekibimizle 7/24 hizmetinizdeyiz."
+        keywords={['ağaç budama', 'ağaç kesme', 'Antalya', 'antalya agac', 'antalya budama', 'antalya agac budama', 'antalya agac kesme','antalya ağaç', 'antalya ağaç budama', 'antalya ağaç kesme' , 'ağaç bakımı', 'ağaç kesme hizmeti', 'ağaç budama hizmeti', 'ağaç kesme fiyatları', 'ağaç budama fiyatları', 'yuksek tirmanis', 'yuksek tirmanis budama', 'yuksek tirmanis kesme', 'yuksek tirmanis bakımı', 'yuksek tirmanis hizmeti', 'yuksek tirmanis fiyatları', 'yuksek tirmanis budama fiyatları', 'yuksek tirmanis kesme fiyatları', 'yuksek tirmanis bakımı fiyatları','tirmanis', 'tirmanis budama', 'tirmanis kesme', 'tirmanis bakımı', 'tirmanis hizmeti', 'tirmanis fiyatları', 'tirmanis budama fiyatları', 'tirmanis kesme fiyatları', 'tirmanis bakımı fiyatları']}
+        canonical="https://antalyaagacbudama.com/products"
+      />
+      {/* Page content */}
+    
+      <section className="py-12">
+        <div className="container mx-auto px-4">
+          <h1 className="text-4xl font-bold mb-12 text-center">Hizmetlerimiz</h1>
+          <ProductList services={services} />
+        </div>
+      </section>
+    </>
   )
 }
